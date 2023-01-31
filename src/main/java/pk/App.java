c0f8d3cfe89a3e092d5e8ce15e369549210a6007
prasanna-761 <123439582+prasanna-761@users.noreply.github.com>
2023-01-31 10:30:42 +0530

    Update App.java

--- src/main/java/pk/App.java
+++ src/main/java/pk/App.java
@@ -50,7 +50,6 @@ public class App
         float gpa = sc.nextFloat();
         STUDENT s = new STUDENT(name,gpa);
         do{
-            try{
                 LOGGER.log(Level.INFO,"\n1.Display GPA 2.Edit GPA 3.Exit");
                 int m = sc.nextInt();
                 if(m == 1){
@@ -64,11 +63,6 @@ public class App
                 else{
                     n = 0;
                 }
-            }catch(Exception e){
-                String s1 = ""+e;
-                LOGGER.info(s1);
-                sc.nextLine();
-            }
         }while(n==1);
     }
 }
